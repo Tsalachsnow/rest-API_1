31dbc41c8624183012903f9ec9697cab0d73b190
TsalachSnow <prosper.amalaha@decagon.dev>
2021-12-30 08:45:01 +0100

    implementation of repository interface and modification of database source from already existing hashmap to sql workbench

--- src/main/java/com/example/rest_api2/controllers/CountryController.java
+++ src/main/java/com/example/rest_api2/controllers/CountryController.java
@@ -4,8 +4,9 @@ package com.example.rest_api2.controllers;
 
 import com.example.rest_api2.models.Country;
 import com.example.rest_api2.services.CountryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -17,32 +18,50 @@ public class CountryController {
     }
 
     @GetMapping("/getcountries")
-    public List getCountries() {
+    public List<Country> getCountries() {
         return countryService.getAllCountries();
     }
 
     @GetMapping("/getcountries/{id}")
-        public Country getCountryById(@PathVariable(value = "id") Long id){
-        return countryService.getCountryById(id);
+    public ResponseEntity<Country> getCountryById(@PathVariable(value = "id") Long id){
+        try {
+            Country country = countryService.getCountryById(id);
+            return new ResponseEntity<Country>(country, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
-        @GetMapping("/getcountries/countryname")
-    public Country getCountryByName(@RequestParam(value = "name") String countryName){
-        return countryService.getCountryByName(countryName);
+    @GetMapping("/getcountries/countryname")
+    public ResponseEntity<Country> getCountryByName(@RequestParam(value = "name") String countryName){
+        try {
+            Country country = countryService.getCountryByName(countryName);
+            return new ResponseEntity<Country>(country, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+    }
 
-        @PostMapping("/addcountry")
+    @PostMapping("/addcountry")
     public Country addCountry(@RequestBody Country country){
         return countryService.addCountry(country);
-        }
+    }
 
-        @PutMapping("/updatecountry")
-    public Country updateCountry(@RequestBody Country country){
-        return countryService.updateCountry(country);
+    @PutMapping("/updatecountry/{id}")
+    public ResponseEntity<Country> updateCountry(@PathVariable(value = "id") Long id, @RequestBody Country country){
+        try{
+            Country existCountry = countryService.getCountryById(id);
+            existCountry.setCountryName(country.getCountryName());
+            existCountry.setCountryCapital(country.getCountryCapital());
+            Country country1 = countryService.updateCountry(existCountry);
+            return new ResponseEntity<Country>(country1, HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
         }
+    }
 
-        @DeleteMapping("/deletecountry/{id}")
+    @DeleteMapping("/deletecountry/{id}")
     public Addresponse deleteCountry(@PathVariable(value="id") Long id){
         return countryService.deleteCountry(id);
-        }
+    }
 }
