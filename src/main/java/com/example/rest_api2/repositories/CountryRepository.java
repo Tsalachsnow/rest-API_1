f7469dcfac074bf6a8eb9f94570bfc17f71a67e3
TsalachSnow <prosper.amalaha@decagon.dev>
2021-12-30 08:40:26 +0100

    testing2

--- src/main/java/com/example/rest_api2/repositories/CountryRepository.java
+++ src/main/java/com/example/rest_api2/repositories/CountryRepository.java
@@ -3,6 +3,6 @@ package com.example.rest_api2.repositories;
 import com.example.rest_api2.models.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class CountryRepository extends JpaRepository<Country, Long> {
+public interface CountryRepository extends JpaRepository<Country, Long> {
 
 }
